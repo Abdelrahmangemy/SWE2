9e96062b6fba67b63c0fa08c800de1df72454aa8
Abdelrahman <abdog1195@gmail.com>
2020-05-11 17:33:08 +0200

    Add HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Products_model;
+use App\Wishlist_model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -11,15 +15,44 @@ class HomeController extends Controller
      *
      * @return void
      */
-    
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
     public function index()
     {
-        return view('welcome');
+        $products = Products_model::all();
+        return view('frontend.home', compact('products'));
     }
+
+    public function shop()
+    {
+         $products = Products_model::all();
+        return view('frontend.shop', compact('products'));
+    }
+
+    public function showCates($id)
+    {
+        $category_products = Products_model::where('category_id',$id)->get();
+
+        $id_ = $id ;
+
+        return view('frontend.category_list_pro',compact('category_products','id_'));
+    }
+
+    public function detailPro($id)
+    {
+        $products = DB::table('products')->where('id',$id)->get();
+
+        return view('frontend.product_detail',compact('products'));
+    }
+
+    
 }
