a486c993d0cd934e0f6c5c57248dce0a3fb6ff95
Mina Shukri <menashokri21@gmail.com>
2020-05-12 12:38:23 +0200

    added indexblade

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.master')
+@section('title','Index Page')
+@section('content')
+
+	<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+		<br><br>
+          <h2 style="text-align: center;">Welcome to our Car Online Shop</h2>
+          <div class="img-responsive text-center"><img src="{{asset('images/logo3.png')}}" width="200px"></div>
+        </main>
+
+
+@endsection
