cb44af63cf5796356d6f65d4feda4345955e8172
Mina Shukri <menashokri21@gmail.com>
2020-05-23 02:37:13 +0200

    commit

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -118,6 +118,79 @@
             </div>
         </div>
     </section> <!--/#cart_items-->
-
+    <section id="do_action">
+        <div class="container">
+            <div class="heading">
+                <h3>What would you like to do next?</h3>
+                <p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="chose_area">
+                        <?php /*      <ul class="user_option">
+                            <li>
+                                <label>Use Coupon Code</label>
+                            </li>
+                            <li>
+                                <input type="text" id="couponCode">
+                            </li>
+                            <li>
+                                <button id="couponBtn">Apply</button>
+                            </li>
+                        </ul>
+                        */?>
+                        <ul class="user_info">
+                            <li class="single_field">
+                                <label>Country:</label>
+                                <select>
+                                    <option>United States</option>
+                                    <option>Bangladesh</option>
+                                    <option>UK</option>
+                                    <option>India</option>
+                                    <option>Pakistan</option>
+                                    <option>Ucrane</option>
+                                    <option>Canada</option>
+                                    <option>Dubai</option>
+                                    <option>Cairo</option>
+                                </select>
+                            </li>
+                            <li class="single_field">
+                                <label>Region / State:</label>
+                                <select>
+                                    <option>Select</option>
+                                    <option>Dhaka</option>
+                                    <option>London</option>
+                                    <option>Dillih</option>
+                                    <option>Lahore</option>
+                                    <option>Alaska</option>
+                                    <option>Canada</option>
+                                    <option>Dubai</option>
+                                    <option>Cairo</option>
+                                </select>
+                            </li>
+                            <li class="single_field zip-field">
+                                <label>Zip Code:</label>
+                                <input type="text">
+                            </li>
+                        </ul>
+                        <a class="btn btn-default update" href="">Get Quotes</a>
+                        <a class="btn btn-default check_out" href="">Continue</a>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="total_area">
+                        <ul>
+                            <li>Cart Sub Total <span>${{Cart::subtotal()}}</span></li>
+                            <li>Eco Tax <span>${{Cart::tax()}}</span></li>
+                            <li>Shipping Cost <span>Free</span></li>
+                            <li>Total <span>${{Cart::total()}}</span></li>
+                        </ul>
+                        <a class="btn btn-default update" href="">Update</a>
+                        <a class="btn btn-default check_out" href="{{url('/')}}/checkout">Check Out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!--/#do_action-->
     <?php } ?>
 @endsection
